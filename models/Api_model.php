61e147343cbb0ede3b4f4b0cd84f6be93302343a
Neto Menezes <61053569+consultor360@users.noreply.github.com>
2024-10-19 13:11:32 -0300

    Funcionando a parte de tokens

--- models/Api_model.php
+++ models/Api_model.php
@@ -13,19 +13,24 @@ class Api_model extends CI_Model
     }
 
     public function get_all_tokens()
-{
-    $this->db->select('*');
-    $this->db->from('tblmulti_pipeline_api_tokens'); // Usando o nome correto da tabela
-    $query = $this->db->get();
-    return $query->result();
-}
+    {
+        $this->db->select('tblmulti_pipeline_api_tokens.*, tblstaff.firstname, tblstaff.lastname');
+        $this->db->from('tblmulti_pipeline_api_tokens');
+        $this->db->join('tblstaff', 'tblstaff.staffid = tblmulti_pipeline_api_tokens.user_id', 'left');
+        $query = $this->db->get();
+        return $query->result();
+    }
 
 public function add_token($data)
 {
-    // Adiciona o user_id ao array de dados
-    $data['user_id'] = $this->session->userdata('user_id'); // Supondo que você armazena o ID do usuário na sessão
-    $this->db->insert('tblmulti_pipeline_api_tokens', $data); // Insere os dados na tabela
-    return $this->db->insert_id(); // Retorna o ID do token adicionado
+    $this->db->insert('tblmulti_pipeline_api_tokens', [
+        'name' => $data['name'],
+        'token' => app_generate_hash(),
+        'user_id' => $data['user_id'],
+        'created_at' => date('Y-m-d H:i:s')
+    ]);
+
+    return $this->db->insert_id();
 }
 
 public function get_tokens() {
