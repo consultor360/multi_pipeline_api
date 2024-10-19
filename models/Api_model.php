fd2d0f7a85e195640e201bd250b8bc17ff463012
Neto Menezes <61053569+consultor360@users.noreply.github.com>
2024-10-19 00:53:56 -0300

    Update Api_model.php

--- models/Api_model.php
+++ models/Api_model.php
@@ -1,4 +1,5 @@
 <?php
+// Caminho: /public_html/modules/multi_pipeline/models/Api_model.php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Api_model extends CI_Model
@@ -17,5 +18,29 @@ class Api_model extends CI_Model
     $this->db->from('tblmulti_pipeline_api_tokens'); // Usando o nome correto da tabela
     $query = $this->db->get();
     return $query->result();
+}
+
+public function add_token($data)
+{
+    // Adiciona o user_id ao array de dados
+    $data['user_id'] = $this->session->userdata('user_id'); // Supondo que você armazena o ID do usuário na sessão
+    $this->db->insert('tblmulti_pipeline_api_tokens', $data); // Insere os dados na tabela
+    return $this->db->insert_id(); // Retorna o ID do token adicionado
+}
+
+public function get_tokens() {
+    $this->db->select('*');
+    $this->db->from('tblmulti_pipeline_api_tokens');
+    $query = $this->db->get();
+    return $query->result();
+}
+
+public function save_token($token, $name) {
+    $data = array(
+        'token' => $token,
+        'name' => $name
+    );
+    $this->db->insert('tblmulti_pipeline_api_tokens', $data);
+    return $this->db->affected_rows() > 0;
 }
 }
